3d3f237b20ba83a9c043fe2c6ed3474fd46e9ed7
placidcy <97069998+placidcy@users.noreply.github.com>
2022-07-21 09:11:28 +0900

    11
    1

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,13 +1,16 @@
 ﻿/// <reference path="navmenu.js" />
-import React, { Component, createRef } from 'react';
+import React, { Component, createRef, useEffect, useRef } from 'react';
 import { BrowserRouter, Route, Router, useHistory } from "react-router-dom";
 import '../css/Login.css';
 import { Link } from "react-router-dom";
 import { UserList } from "./UserList";
 
+// 유저 데이터 불러오고 사용자 리스트를 화면에 렌더링하기
+
 export class Login extends Component{
     constructor(props) {
         super(props);
+
     }
 
     clickLogin = () => {
@@ -24,19 +27,6 @@ export class Login extends Component{
 
         this.doLogin(id.value, password.value);
     }
-    
-    /*async doLogin(id, password) {
-        const res = await fetch('/User/Test/', {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                id: id,
-            }),
-        });
-    }*/
 
     async doLogin(id, password) {
         // UnserInfoContaller/requestUserData 함수
